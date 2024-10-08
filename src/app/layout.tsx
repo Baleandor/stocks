ea1a3e5fde55e4b36411bad233726fa32da8fc9f
Baleandor <overpoweredmedia@gmail.com>
2024-10-08 13:57:21 +0300

    Redux store cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import Header from "~/components/Header";
-import { store } from "../store/store";
-import { Provider } from "react-redux";
+import { ReduxProvider } from "./StoreProvider";
 export const metadata: Metadata = {
   title: "Stocks",
   description: "Stock tracking app",
@@ -15,13 +14,11 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <Provider store={store}>
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-          <Header />
-          {children}
-        </body>
-      </html>
-    </Provider>
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <Header />
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
+    </html>
   );
 }
--- src/store/stock-slice.ts
+++ src/store/stock-slice.ts
@@ -1,3 +1,4 @@
+"use client";
 import { createSlice } from "@reduxjs/toolkit";
 import { StockSliceType } from "~/utils/types";
 
